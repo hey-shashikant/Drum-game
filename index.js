f6128a2fbaf4b0f25fcb68b47ae43a83be8b23e4
Shashikant Solanki <90645693+hey-shashikant@users.noreply.github.com>
2022-05-06 00:02:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var length = document.querySelectorAll(".drum").length;
+for(var i = 0; i<length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+        var keyword = this.innerHTML;
+        makeSound(keyword);
+        buttonAnimation(keyword);
+    } );
+
+    document.addEventListener("keypress", function(event) {
+        var keyword = event.key;
+        makeSound(keyword);
+        buttonAnimation(keyword);
+    } )
+}
+
+function makeSound(keyword){
+    switch (keyword) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();          
+            break;
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();          
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();          
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();          
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();          
+            break;
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();          
+            break;
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();          
+            break;
+        default:
+            break;
+    }
+}
+
+
+function buttonAnimation(currentKey){
+    var activebutton = document.querySelector("." + currentKey);
+    activebutton.classList.add("pressed");
+
+    setTimeout(function () {
+        activebutton.classList.remove("pressed");
+    },100);
+}
+
